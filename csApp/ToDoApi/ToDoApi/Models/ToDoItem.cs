cd9bea4e65d03987bf75732280b7d15e53e5628c
George Johnson <georgemjohnson11@gmail.com>
2018-10-20 15:19:01 -0600

    ToDoList API with Visual Studio Community 2017 https://github.com/aspnet/Docs/blob/master/aspnetcore/tutorials/first-web-api.md

---
+++ csApp/ToDoApi/ToDoApi/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ToDoApi.Models
+{
+    public class ToDoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public ToDoItem()
+        {
+            
+        }
+    }
+}
